f5340d0855a0d989582adcfdc0764884edafaf69
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2020-12-10 20:11:04 +0530

    Create IsBST.cpp

---
+++ BST/IsBST.cpp
@@ -0,0 +1,7 @@
+bool isBST(BinaryTreeNode<int> *root)
+{
+	if(root==NULL) return true;
+	if(root->left && root->left->data > root->data) return false;
+	if(root->right && root->right->data < root->data) return false;
+	return isBST(root->left) && isBST(root->right);
+}
