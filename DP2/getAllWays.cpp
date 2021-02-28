51547f93c0aaff57845bb7d7c61beb285e0e50a2
Naman Goyal <64425754+Namangoyalng@users.noreply.github.com>
2021-03-01 01:15:18 +0530

    Update getAllWays.cpp

--- DP2/getAllWays.cpp
+++ DP2/getAllWays.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int getAllWays(int a,int b,int num)
 {
-    int val=x-pow(num,b);
+    int val=a-pow(num,b);
     if(val==0) return 1;
     else if(val < 0) return 0;
     return getAllWays(val,b,num+1)+getAllWays(a,b,num+1);
