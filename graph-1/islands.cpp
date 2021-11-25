e242593cff6b12e5544a7dbbe4852bc9167de0a1
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2021-11-25 19:04:34 +0530

    Upgraded Code
    Optimized code

--- graph-1/islands.cpp
+++ graph-1/islands.cpp
@@ -1,58 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-void BFS(bool **edge,int n,int sv,unordered_map<int,bool>& visited){
-    queue<int> q;
-    q.push(sv);
-    visited[sv]=1;
-    while(!q.empty()){
-        int front=q.front();
-        q.pop();
-        for(int i=0;i<n;i++){
-            if(!visited[i] && edge[front][i] && i!=front){
-                q.push(i);
-                visited[i]=1;
-            }
-        }
-    }
+const int N= 1005;
+vector<int> adj[N];
+vector<bool> vis(N,0);
+void dfs(int v){
+    vis[v]=1;
+    for(auto &u:adj[v]) if(!vis[u]) dfs(u);
 }
-void BFS(bool **edge,int n){
-    unordered_map<int,bool> visited;
-    for(int i=0;i<n;i++){
-        visited[i]=0;
-    }
-    int ans=0;
-    for(int i=0;i<n;i++){
-        if(!visited[i]){
-            BFS(edge,n,i,visited);
-            ans++;
-        }
-    }
-    cout<<ans;
-}
-int main(){
-    int n,e;
-    cin>>n>>e;
-    bool **edge=new bool*[n];
-    for(int i=0;i<n;i++){
-        edge[i]=new bool[n];
-        for(int j=0;j<n;j++){
-            edge[i][j]=0;
-        }
-    }
+int main() {
+    int v,e;
+    scanf("%d %d",&v,&e);
     for(int i=0;i<e;i++){
         int f,s;
-        cin>>f>>s;
-        edge[f][s]=1;
-        edge[s][f]=1;
+        scanf("%d %d",&f,&s);
+        adj[f].push_back(s);
+        adj[s].push_back(f);
     }
-    unordered_map<int,bool> visited;
-    for(int i=0;i<n;i++){
-        visited[i]=0;
-    }
-    BFS(edge,n);
-    //deleting dynammic memory
-    for(int i=0;i<n;i++){
-        delete[] edge[i];
-    }
-    delete[] edge;
+    int cnt=0;
+    for(int i=0;i<v;i++) if(!vis[i]) dfs(i),cnt++;
+    printf("%d",cnt);
 }
