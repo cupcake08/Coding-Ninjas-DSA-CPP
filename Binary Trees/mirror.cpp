9efa4d85eb921f1995f2a99e2845ad555ab2d361
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2021-11-29 13:29:30 +0530

    Update mirror.cpp

--- Binary Trees/mirror.cpp
+++ Binary Trees/mirror.cpp
@@ -1,12 +1,14 @@
-void mirrorBinaryTree(BinaryTreeNode<int> *root)
-{
-    if (root == nullptr)
-    {
-        return;
+#define BTN BinaryTreeNode<int>*
+void swap(BTN root){
+    if(root->left && root->right){
+        BTN temp = root->left;
+    	root->left = root->right;
+    	root->right = temp;
     }
-    BinaryTreeNode<int> *temp=root->left;
-    root->left=root->right;
-    root->right=temp;
+}
+void mirrorBinaryTree(BTN root) {
+    if(root == NULL) return;
+    swap(root);
     mirrorBinaryTree(root->left);
     mirrorBinaryTree(root->right);
 }
