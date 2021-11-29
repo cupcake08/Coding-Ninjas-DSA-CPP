80226e931825c3810613306d80cfd6649ace67bf
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2021-11-29 08:56:27 +0530

    Update StructurallyIdentical.cpp

--- Tree/StructurallyIdentical.cpp
+++ Tree/StructurallyIdentical.cpp
@@ -1,23 +1,9 @@
-
 bool areIdentical(TreeNode<int> *root1, TreeNode<int> * root2) {
-    if((root1==nullptr && root2 !=nullptr) || (root1 !=nullptr && root2==nullptr) )
-    {
-        return false;
-    }
-    if(root1->data != root2->data)
-    {
-        return false;
-    }
-    if(root1->children.size()!=root2->children.size())
-    {
-        return false;
-    }
-    for(int i=0;i<root1->children.size();i++)
-    {
-        if(!areIdentical(root1->children[i],root2->children[i]))
-        {
-            return false;
-        }
-    }
-    return true;
+    if(root1 == NULL && root2 != NULL) return false;
+    if(root1 != NULL && root2 == NULL) return false;
+    if(root1 == NULL && root2 == NULL) return 1;
+    if(root1->data != root2->data) return 0;
+    if(root1->children.size() != root2->children.size()) return 0;
+    for(int i=0;i<root1->children.size();i++) if(!areIdentical(root1->children[i],root2->children[i])) return 0;
+    return 1;
 }
