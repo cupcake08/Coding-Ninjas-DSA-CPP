ed64da7b66ccfb62debc8546d41b7c20f9c7c44f
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2021-11-29 15:04:15 +0530

    Update nodeWithoutSiblings.cpp

--- Binary Trees/nodeWithoutSiblings.cpp
+++ Binary Trees/nodeWithoutSiblings.cpp
@@ -1,13 +1,7 @@
 void printNodesWithoutSibling(BinaryTreeNode<int> *root) {
-    if(root==nullptr) return;
-    if(root->left!=nullptr && root->right==nullptr)
-    {
-        cout<<root->left->data<<" ";
-    }
-    if(root->right!=nullptr && root->left == nullptr)
-    {
-        cout<<root->right->data<<" ";
-    }
+    if(root == NULL) return;
+    if(root->left && !root->right) printf("%d ",root->left->data);
+    if(root->right && !root->left) printf("%d ",root->right->data);
     printNodesWithoutSibling(root->left);
     printNodesWithoutSibling(root->right);
 }
