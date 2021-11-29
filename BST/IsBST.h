0ad1739c7c8c086d0cd9aeee501a61fc9ab75099
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2021-11-29 15:34:20 +0530

    Update and rename IsBST.cpp to IsBST.h

--- BST/IsBST.h
+++ BST/IsBST.h
@@ -3,5 +3,5 @@ bool isBST(BinaryTreeNode<int> *root)
 	if(root==NULL) return true;
 	if(root->left && root->left->data > root->data) return false;
 	if(root->right && root->right->data < root->data) return false;
-	return isBST(root->left) && isBST(root->right);
+	return isBST(root->left) & isBST(root->right);  //you can use '&&' operator here as well
 }
