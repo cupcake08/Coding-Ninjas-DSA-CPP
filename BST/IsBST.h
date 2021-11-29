945f98a5e247e9974ce7472a85a00f25cc962b06
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2021-11-29 20:18:36 +0530

    Update IsBST.h

--- BST/IsBST.h
+++ BST/IsBST.h
@@ -1,7 +1,7 @@
 bool isBST(BinaryTreeNode<int> *root)
 {
-	if(root==NULL) return true;
-	if(root->left && root->left->data > root->data) return false;
-	if(root->right && root->right->data < root->data) return false;
+	if(root==NULL) return 1;
+	if(root->left && root->left->data > root->data) return 0;
+	if(root->right && root->right->data < root->data) return 0;
 	return isBST(root->left) & isBST(root->right);  //you can use '&&' operator here as well
 }
