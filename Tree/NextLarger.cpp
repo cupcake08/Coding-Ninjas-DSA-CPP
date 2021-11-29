3a7bbf8b12a356b4181221d76ad2dd00471bebe4
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2021-11-29 09:02:45 +0530

    Update NextLarger.cpp

--- Tree/NextLarger.cpp
+++ Tree/NextLarger.cpp
@@ -1,20 +1,11 @@
-TreeNode<int>* getNextLargerElement(TreeNode<int> *root, int n) {
-	if(root==nullptr) return root;
-	TreeNode<int> *ans=nullptr;	
-	if(root->data > n)
-	{
-		ans=root;
-	}
-	for(int i=0;i<root->children.size();i++)
-	{
-		TreeNode<int> *temp=getNextLargerElement(root->children[i],n);
-		if(ans==nullptr)
-		{
-			ans= temp;
-		}else if(temp != nullptr && temp->data < ans->data)
-		{
-			ans = temp;
-		}
-	}
-	return ans;
+TreeNode<int>* getNextLargerElement(TreeNode<int>* root, int x) {
+    if(root == NULL) return root;
+    TreeNode<int> *ans = NULL;
+    if(root->data > x) ans = root;
+    for(auto &node:root->children){
+        TreeNode<int> *temp = getNextLargerElement(node,x);
+        if(ans == NULL) ans = temp;
+        else if(temp != NULL && temp->data < ans->data) ans = temp;
+    }
+    return ans;
 }
