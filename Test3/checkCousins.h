9e4e5d6417166b34615b8dfc43f60862e38535a3
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2021-11-29 21:34:26 +0530

    Update checkCousins.h

--- Test3/checkCousins.h
+++ Test3/checkCousins.h
@@ -1,3 +1,19 @@
+/*
+
+Given the binary Tree and two nodes say ‘p’ and ‘q’. Determine whether the two nodes are cousins of each other or not. 
+Two nodes are said to be cousins of each other if they are at same level of the Binary Tree and have different parents.
+
+(Do it in O(n)).
+Sample Input :
+5 6 10 2 3 4 -1 -1 -1 -1 9 -1 -1 -1 -1
+2
+4
+
+Sample Output :
+true
+
+*/
+
 #define _VI_ vector<int>
 #define _BTN_ BinaryTreeNode<int>*
 
